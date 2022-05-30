4b2edb9853bdfee48856ad29a0a45dff7b7910fd
kennart <kennartfoundation@gmail.com>
2022-05-29 21:34:04 -0500

    added Read me and try.html
    description

---
+++ app.py
@@ -0,0 +1,34 @@
+import sys
+from tkinter import *
+import customtkinter 
+from tkinter import messagebox
+
+
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme('blue')
+
+
+
+app = customtkinter.CTk()
+app.geometry('400x400')
+app.title('My desktop app')
+# app.iconbitmap('/home/kenneth/Desktop/the git/Desktop_Application/KM.ico')
+
+
+
+
+
+app.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
